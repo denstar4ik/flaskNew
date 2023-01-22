6b2af9b529eec23821f74e9a13038d89e9e9365e
test2 <test2@YANDEX.RU>
2023-01-22 19:24:35 +0300

    12

--- flask_app.py
+++ flask_app.py
@@ -164,7 +164,7 @@ if __name__ == '__main__':
 @app.route('/update_server', methods=['POST', 'GET'])
 def webhook():
     if request.method == 'POST':
-        repo = git.Repo('/home/mypythonprojects/flaskNew')
+        repo = git.Repo('/home/sealeks/flaskNew')
         origin = repo.remotes.origin
         origin.pull()
         return 'Сайт обновился', 200
